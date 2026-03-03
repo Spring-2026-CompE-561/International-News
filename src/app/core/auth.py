a1ba342fb810c69e7f29fc5241f0d51c90d88eea
Will Villator0 <wvillatoro0477@sdsu.edu>
2026-03-02 22:09:02 -0800

    Added core auth

---
+++ src/app/core/auth.py
@@ -0,0 +1,41 @@
+from datetime import UTC, datetime, timedelta
+
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from jwt import PyJWTError
+from pwdlib import PasswordHash
+
+
+from app.core.settings import settings
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(UTC) + expires_delta
+    else:
+        expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+
+def get_password_hash(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+def verify_token(token: str) -> dict | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except PyJWTError:
+        return None
+    else: 
+        return payload
