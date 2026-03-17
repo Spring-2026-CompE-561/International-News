42c1386ded2fbb316a3b49179514444cd289036b
Trevor Plott <Trevorplott@icloud.com>
2026-03-17 10:12:27 -0700

    Added the model for region, according to the google doc

---
+++ src/app/models/region.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+
+class Region(Base):
+    __tablename__ = "regions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    description = Column(String, nullable=True)
+
+    articles = relationship("Article", back_populates="region")
+    sources = relationship("Source", back_populates="region")
