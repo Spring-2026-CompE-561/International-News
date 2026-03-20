2db75e3071a196ad401d2c2c482b720730fda948
Trevor Plott <Trevorplott@icloud.com>
2026-03-19 20:02:10 -0700

    fixed the routes for articles and topics to match the doc, so it is easy to work on when ready

--- src/app/routes/topics.py
+++ src/app/routes/topics.py
@@ -6,3 +6,18 @@ api_router = APIRouter(prefix="/topics", tags=["topics"])
 @api_router.get("")
 async def get_topics():
     return {"message": "Get topics route"}
+
+
+@api_router.get("/trending")
+async def get_trending_topics():
+    return {"message": "Get treding topics"}
+
+
+@api_router.get("/{id}")
+async def get_topics_by_id(id: int):
+    return {"message": f"Get topics by {id}"}
+
+
+@api_router.get("/{id}/coverage")
+async def get_topics_coverage(id: int):
+    return {"message": f"coverage for topic {id}"}
